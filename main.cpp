83458c39190ee3fd2d03f666bf96a606660ed238
Theshakyhorse <amaximus.sierra@gmail.com>
2026-02-22 00:53:51 -0800

    second

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 using namespace std;
 
+const int NUM_EMPLOYEES = 3;
+
+//struct representing employees at a company
+struct Employee {
+    string name;
+    int age;
+    int * degrees;
+
+    ~Employee() {
+        if (degrees)
+            delete [] degrees;
+        degrees = nullptr;
+    }
+};
+
 int main(){
-    
+    Employee *list = new Employee[NUM_EMPLOYEES];
+
     return 0;
+}
+
+void inputEmployee(Employee * sptr) {
+    
 }
